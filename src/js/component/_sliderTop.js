2fbabb1ab10b507a56af9c3701e6a071fce2a280
Denis Sinkevich <sinkevich_denis@inbox.ru>
2018-12-18 22:23:05 +0300

    slider's moving in process

--- src/js/component/_sliderTop.js
+++ src/js/component/_sliderTop.js
@@ -1,9 +1,20 @@
 (function (){
-	var borderWidth;
-	
-	//change shadow's size on the slaider
+	var borderWidth,
+	slideElement = document.querySelector(".sliderTop_images"),
+	slideSize,
+	currentPosition = 0,
+	oldPositiion,
+	timePeriod = 3000,
+	indexInterval;
+
+	var arrayDot= document.querySelectorAll(".sliderTop_dot"),
+	arrayLayer= document.querySelectorAll(".sliderTop_layer"),
+	classDot = "sliderTop_dot__visible",
+	classLayer= "style_layer__visible";
+
+	//change shadow's size on the slider
 	window.addEventListener("resize", resizeShadow);
-	window.addEventListener("load", resizeShadow) ;
+	window.addEventListener("load", resizeShadow);
 
 	function resizeShadow ()
 	{
@@ -14,5 +25,78 @@
 		$(".sliderTop_shadow").css("border-right-width", borderWidth);
 	}
 
+	//slider's work - change images
+	window.addEventListener("load", startSlider);
+	window.addEventListener("resize", restartSlider);
+
+	function restartSlider()
+	{
+		slideSize = slideElement.clientHeight+1;
+		console.log(slideSize);
+		/*console.log(slideElement.getBoundingClientRect());*/
+/*		clearInterval(indexInterval);
+		slideSize = slideElement.clientHeight+1;
+		while (currentPosition % slideSize !== 0)
+		{
+			currentPosition++;
+			slideElement.scroll({top: currentPosition, behavior: "auto" });
+		}
+		startSlider();*/
+	}
+
+	function startSlider()
+	{
+		slideSize = slideElement.clientHeight-1;
+		indexInterval = setInterval(function()
+		{
+			currentPosition += slideSize;
+			if (currentPosition >= (slideElement.scrollHeight - 30))
+			//30 - это погрешность, на некоторых разрешениях получается не целый размер картинки,
+			//	 и слайдер делает лишний переход 
+			{
+				currentPosition = slideSize;
+				slideElement.scroll({top: 0, behavior: "auto"});
+			}
+			slideElement.scroll({top: currentPosition, behavior: "smooth" });
+			changeText();
+		}, timePeriod);
+	}
+
+	// change text and dots on slides
+	function changeText()
+	{	
+
+		if (currentPosition>=slideSize && currentPosition<slideSize*2)
+		{
+			console.log("!!!!!!!",currentPosition, slideSize, slideSize*2);
+			console.log("11111", arrayDot[0].getBoundingClientRect());
+			arrayDot[0].classList.toggle(classDot);
+			arrayDot[1].classList.toggle(classDot);
+			arrayLayer[0].classList.toggle(classLayer);
+			arrayLayer[1].classList.toggle(classLayer);
+		}
+		if (currentPosition>=slideSize*2 && currentPosition<slideSize*3)
+		{
+			console.log("!!!!!!!",currentPosition, slideSize*2, slideSize*3);
+			arrayDot[1].classList.toggle(classDot);
+			arrayDot[2].classList.toggle(classDot);
+			arrayLayer[1].classList.toggle(classLayer);
+			arrayLayer[2].classList.toggle(classLayer);
+		}
+		if (currentPosition>=slideSize*3 && currentPosition<slideSize*4)
+		{
+			console.log("!!!!!!!",currentPosition, slideSize*3, slideSize*4);
+			arrayDot[2].classList.toggle(classDot);
+			arrayDot[0].classList.toggle(classDot);
+			arrayLayer[2].classList.toggle(classLayer);
+			arrayLayer[0].classList.toggle(classLayer);
+		}
+	}
+
+	//script for slider's control buttons
+	document.querySelectorAll(".sliderTop_buttons").forEach(function (element)
+	{
+
+	})
 
 })();
