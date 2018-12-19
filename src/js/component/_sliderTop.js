679174d0fca48664c23bba754656d7db93f46713
Denis Sinkevich <sinkevich_denis@inbox.ru>
2018-12-19 14:26:03 +0300

    finish slider

--- src/js/component/_sliderTop.js
+++ src/js/component/_sliderTop.js
@@ -3,12 +3,13 @@
 	slideElement = document.querySelector(".sliderTop_images"),
 	slideSize,
 	currentPosition = 0,
-	oldPositiion,
-	timePeriod = 3000,
-	indexInterval;
+	normalTimePeriod = 3000,
+	resizeTimePeriod = 100,
+	indexInterval,
+	indexImage = 0;
 
-	var arrayDot= document.querySelectorAll(".sliderTop_dot"),
-	arrayLayer= document.querySelectorAll(".sliderTop_layer"),
+	var arrayDot= $(".sliderTop_dot"),
+	arrayLayer= $(".sliderTop_layer"),
 	classDot = "sliderTop_dot__visible",
 	classLayer= "style_layer__visible";
 
@@ -26,27 +27,21 @@
 	}
 
 	//slider's work - change images
-	window.addEventListener("load", startSlider);
+	window.addEventListener("load", restartSlider);
 	window.addEventListener("resize", restartSlider);
 
 	function restartSlider()
 	{
-		slideSize = slideElement.clientHeight+1;
-		console.log(slideSize);
-		/*console.log(slideElement.getBoundingClientRect());*/
-/*		clearInterval(indexInterval);
-		slideSize = slideElement.clientHeight+1;
-		while (currentPosition % slideSize !== 0)
-		{
-			currentPosition++;
-			slideElement.scroll({top: currentPosition, behavior: "auto" });
-		}
-		startSlider();*/
+		clearInterval(indexInterval);
+		slideSize = slideElement.clientHeight;
+		currentPosition = indexImage*slideSize;
+		startSlider(resizeTimePeriod, "auto");
 	}
 
-	function startSlider()
-	{
-		slideSize = slideElement.clientHeight-1;
+	function startSlider(time, type)
+	 {
+		slideSize = slideElement.clientHeight;
+
 		indexInterval = setInterval(function()
 		{
 			currentPosition += slideSize;
@@ -56,36 +51,40 @@
 			{
 				currentPosition = slideSize;
 				slideElement.scroll({top: 0, behavior: "auto"});
+				indexImage = 0;
 			}
-			slideElement.scroll({top: currentPosition, behavior: "smooth" });
+			slideElement.scroll({top: currentPosition, behavior: type});
+			indexImage++;
 			changeText();
-		}, timePeriod);
+
+			if (time === resizeTimePeriod)
+			{
+				clearInterval(indexInterval);
+				startSlider(normalTimePeriod, "smooth");
+			}
+		}, time);
 	}
 
 	// change text and dots on slides
 	function changeText()
 	{	
 
-		if (currentPosition>=slideSize && currentPosition<slideSize*2)
+		if (indexImage === 1)
 		{
-			console.log("!!!!!!!",currentPosition, slideSize, slideSize*2);
-			console.log("11111", arrayDot[0].getBoundingClientRect());
 			arrayDot[0].classList.toggle(classDot);
 			arrayDot[1].classList.toggle(classDot);
 			arrayLayer[0].classList.toggle(classLayer);
 			arrayLayer[1].classList.toggle(classLayer);
 		}
-		if (currentPosition>=slideSize*2 && currentPosition<slideSize*3)
+		if (indexImage === 2)
 		{
-			console.log("!!!!!!!",currentPosition, slideSize*2, slideSize*3);
 			arrayDot[1].classList.toggle(classDot);
 			arrayDot[2].classList.toggle(classDot);
 			arrayLayer[1].classList.toggle(classLayer);
 			arrayLayer[2].classList.toggle(classLayer);
 		}
-		if (currentPosition>=slideSize*3 && currentPosition<slideSize*4)
+		if (indexImage === 3)
 		{
-			console.log("!!!!!!!",currentPosition, slideSize*3, slideSize*4);
 			arrayDot[2].classList.toggle(classDot);
 			arrayDot[0].classList.toggle(classDot);
 			arrayLayer[2].classList.toggle(classLayer);
@@ -96,7 +95,16 @@
 	//script for slider's control buttons
 	document.querySelectorAll(".sliderTop_buttons").forEach(function (element)
 	{
-
+		document.querySelector(".go-up").onclick = function ()
+		{
+			currentPosition += slideSize;
+			restartSlider();
+		};
+		document.querySelector(".go-down").onclick = function ()
+		{
+			currentPosition -= currentPosition - slideSize;
+			restartSlider();
+		};	
 	})
 
 })();
